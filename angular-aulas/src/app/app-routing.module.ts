916d5b057920a273f7c816d8c527e0b3c25be3ea
Iván Rodríguez Ruiz <ivanrguez1@yahoo.es>
2024-02-11 21:04:46 +0100

    [#] Aplicación Angular Aulas

--- angular-aulas/src/app/app-routing.module.ts
+++ angular-aulas/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AulaListComponent } from './components/aula-list/aula-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/verAulas', component: AulaListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
